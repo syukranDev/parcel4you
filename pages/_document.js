5489ded4303091831991627b49fe0968bc735db4
syukranDev <syukran.dev@gmail.com>
2023-05-02 00:44:27 +0800

    first commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,10 +1,12 @@
+import Navbar from '@/components/Navbar'
 import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <Navbar/>
+      <body className='body-bg'>
         <Main />
         <NextScript />
       </body>
---
+++ pages/landing.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const landing = () => {
+  return (
+    <section className='container mx-auto text-center py-20'>
+        <h3 className='text-white text-9xl font-semibold mb-10'>parcel4u</h3>
+        <p className='text-gray-300 text-xl'> 
+            Your one-stop-shop for parcel delivery fees comparisons.
+        </p>
+
+    </section>
+  )
+}
+
+export default landing
